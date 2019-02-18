68d2323355b5817b2479ad67ada9d9e6135fc459
Enrique <enriquealexy@gmail.com>
2019-02-18 19:13:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = "HELLO!"
   var lowercase = 'hello!'
-  string.toLowerCase() === lowercase
+  if(string.toLowerCase() === lowercase){
+    return
+  }
 }
